b1ded8c88660946c37b292b5b6f2513964b15379
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2017-05-11 21:27:31 +0000

    fix NLU_DMComponent to actually be stateful (in order to be able to manage connections)

--- src/main/java/edu/cmu/inmind/multiuser/sara/component/NLU_DMComponent.java
+++ src/main/java/edu/cmu/inmind/multiuser/sara/component/NLU_DMComponent.java
@@ -7,6 +7,7 @@ import edu.cmu.inmind.multiuser.common.model.DMOutput;
 import edu.cmu.inmind.multiuser.common.model.SaraInput;
 import edu.cmu.inmind.multiuser.common.model.SaraOutput;
 import edu.cmu.inmind.multiuser.common.model.UserIntent;
+import edu.cmu.inmind.multiuser.common.model.ASROutput;
 import edu.cmu.inmind.multiuser.controller.blackboard.BlackboardEvent;
 import edu.cmu.inmind.multiuser.controller.blackboard.BlackboardSubscription;
 import edu.cmu.inmind.multiuser.controller.communication.ClientCommController;
@@ -15,20 +16,18 @@ import edu.cmu.inmind.multiuser.controller.communication.SessionMessage;
 import edu.cmu.inmind.multiuser.controller.communication.ZMsgWrapper;
 import edu.cmu.inmind.multiuser.controller.log.Log4J;
 import edu.cmu.inmind.multiuser.controller.plugin.PluggableComponent;
-import edu.cmu.inmind.multiuser.controller.plugin.StatelessComponent;
+import edu.cmu.inmind.multiuser.controller.plugin.StatefulComponent;
 
 import java.util.ArrayList;
 
 /**
  * Created by oscarr on 3/7/17.
  */
-@StatelessComponent
-@BlackboardSubscription( messages = {SaraCons.MSG_ASR})
+@StatefulComponent
+@BlackboardSubscription( messages = {SaraCons.MSG_ASR, "MSG_START_SESSION"} )
 public class NLU_DMComponent extends PluggableComponent {
     private ClientCommController commController;
-    //private final String pythonDialogueAddress = "tcp://128.237.220.225:5590";
     private final String pythonDialogueAddress = Utils.getProperty("pythonDialogueAddress");
-    private String serviceName;
 
     @Override
     public void startUp(){
@@ -36,6 +35,7 @@ public class NLU_DMComponent extends PluggableComponent {
 //        postCreate();
 //        postCreateblackboard().post(this, SaraCons.MSG_ASR, "Hello");
         //blackboard().post(this, SaraCons.MSG_ASR, "Hello");
+	Log4J.info(this, "NLU_DMComponent: startup has finished.");
     }
 
     @Override
@@ -49,10 +49,11 @@ public class NLU_DMComponent extends PluggableComponent {
     }
 
     public void postCreate(){
-        serviceName = getSessionId();
+        Log4J.info(this, "postCreate is called.");
+        Log4J.info(this, "sessionID is " + getSessionId());
         String[] msgSubscriptions = {"MSG_ASR"};
         ZMsgWrapper msgWrapper = new ZMsgWrapper();
-        commController = new ClientCommController(pythonDialogueAddress, serviceName,
+        commController = new ClientCommController(pythonDialogueAddress, getSessionId(),
                 Utils.getProperty("dialogueAddress"), Constants.REQUEST_CONNECT, msgWrapper, msgSubscriptions);
     }
 
@@ -67,13 +68,23 @@ public class NLU_DMComponent extends PluggableComponent {
     }
 
     public void onEvent(BlackboardEvent blackboardEvent) {
+        Log4J.debug(this, "received " + blackboardEvent.toString());
         // let's forward the ASR message to DialoguePython:
-        commController.send( serviceName, blackboardEvent.getElement() );
-
+        if (blackboardEvent.getId().equals("MSG_START_SESSION")){
+		ASROutput initialGreeting = new ASROutput("Hello", 1.0);
+		commController.send( getSessionId(), initialGreeting );
+                commController.send( getSessionId(), initialGreeting );
+		Log4J.debug(this, "Sending Initial Greeting");
+	} else {
+		Log4J.debug(this, "sending on " + blackboardEvent.toString() );
+		commController.send( getSessionId(), blackboardEvent.getElement() );
+//		Log4J.debug(this, "now something was sent");
+	}
         // here we receive the response from DialoguePython:
         commController.receive(new ResponseListener() {
             @Override
             public void process(String message) {
+		Log4J.debug(NLU_DMComponent.this, "I've received: " + message);
                 blackboard().post( NLU_DMComponent.this, SaraCons.MSG_DM,
                         Utils.fromJson( message, DMOutput.class ));
             }
@@ -84,7 +95,7 @@ public class NLU_DMComponent extends PluggableComponent {
     public void shutDown(){
         SessionMessage sessionMessage = new SessionMessage();
         sessionMessage.setRequestType( Constants.REQUEST_DISCONNECT );
-        commController.send( serviceName, sessionMessage );
+        commController.send( getSessionId(), sessionMessage );
     }
 
 }
