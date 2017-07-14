39b95543ebf490dc6338cba9a2cba4cf8ac85634
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-07-14 16:07:39 -0400

    include the CSCComponent

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 date=`date --iso-8601=seconds`
-bash ./gradlew build && java -jar build/libs/sara-with-dependencies-2.0.jar  > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
+bash ./gradlew build -x test && java -jar build/libs/sara-with-dependencies-2.0.jar  > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
--- src/main/java/edu/cmu/inmind/multiuser/sara/examples/Ex15_WholePipeline.java
+++ src/main/java/edu/cmu/inmind/multiuser/sara/examples/Ex15_WholePipeline.java
@@ -4,6 +4,7 @@ import edu.cmu.inmind.multiuser.common.SaraCons;
 import edu.cmu.inmind.multiuser.controller.plugin.PluginModule;
 import edu.cmu.inmind.multiuser.sara.component.*;
 import edu.cmu.inmind.multiuser.sara.orchestrator.SaraOrchestratorEx15;
+import edu.cmu.lti.rapport.pipline.csc.ConversationalStrategyUtil;
 
 /**
  * Created by oscarr on 4/10/17.
@@ -15,6 +16,7 @@ import edu.cmu.inmind.multiuser.sara.orchestrator.SaraOrchestratorEx15;
 public class Ex15_WholePipeline extends MainClass {
 
     public static void main(String args[]) {
+        ConversationalStrategyUtil.preloadRecipes();
         new Ex15_WholePipeline().execute();
     }
 
@@ -23,14 +25,14 @@ public class Ex15_WholePipeline extends MainClass {
         return new PluginModule[]{
                 new PluginModule.Builder(SaraOrchestratorEx15.class)
                         //comment out the line below if you want to use remote DialogueSystem
-                        //.addPlugin(NLUComponent.class, SaraCons.ID_NLU)
-                        //.addPlugin(CSCComponent.class, SaraCons.ID_CSC)
+                        .addPlugin(NLUComponent.class, SaraCons.ID_NLU)
+                        .addPlugin(CSCComponent.class, SaraCons.ID_CSC)
                         //.addPlugin(NLU_DMComponent.class, SaraCons.ID_NLU)
                         //.addPlugin(RapportEstimator.class, SaraCons.ID_RPT)
-                        //.addPlugin(TaskReasonerComponent.class, SaraCons.ID_DM)
+                        .addPlugin(TaskReasonerComponent.class, SaraCons.ID_DM)
                         .addPlugin(SocialReasonerComponent.class, SaraCons.ID_SR)
                         .addPlugin(NLGComponent.class, SaraCons.ID_NLG)
-                        //.addPlugin(OpenFaceComponent.class, SaraCons.ID_OF)
+                        .addPlugin(OpenFaceComponent.class, SaraCons.ID_OF)
                         //.addPlugin(R5StreamComponent.class, SaraCons.ID_R5)
                         //.addPlugin(UserModelComponent.class, SaraCons.ID_UM)
                         .build()
