71cb184253ef15c1388d86da2c5119e569b81b2f
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-07-29 11:11:29 -0400

    resolve nasty dependency issue between beat and csc, osx-compatible date arguments

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-date=`date --iso-8601=seconds`
+#date=`date --iso-8601=seconds`
+date=`date -u +"%Y-%m-%dT%H:%M:%S"`
 bash ./gradlew build -x test && java -jar build/libs/sara-with-dependencies-2.0.jar  > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
