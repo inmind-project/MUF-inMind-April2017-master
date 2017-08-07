04bf68c85cecd665cf4819fb1837d07c06ffd9b2
Yoichi Matsuyama <yoichim@cs.cmu.edu>
2017-08-07 00:28:17 -0400

    reverse connection: See Ex16_MasterCallsSlaves

--- src/main/java/edu/cmu/inmind/multiuser/sara/examples/Ex16_MasterCallsSlaves.java
+++ src/main/java/edu/cmu/inmind/multiuser/sara/examples/Ex16_MasterCallsSlaves.java
@@ -1,23 +1,37 @@
 package edu.cmu.inmind.multiuser.sara.examples;
 
 import edu.cmu.inmind.multiuser.common.SaraCons;
+import edu.cmu.inmind.multiuser.controller.ShutdownHook;
 import edu.cmu.inmind.multiuser.controller.plugin.PluginModule;
+import edu.cmu.inmind.multiuser.controller.resources.Config;
 import edu.cmu.inmind.multiuser.sara.component.*;
 import edu.cmu.inmind.multiuser.sara.orchestrator.SaraOrchestratorEx15;
-//import edu.cmu.lti.rapport.pipline.csc.ConversationalStrategyUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by oscarr on 4/10/17.
+ * Created by oscarr on 8/02/17.
  *
  * This example illustrates the whole pipeline:
  * AndroidClient (ASR) -> DialogueSystem (NLU) -> TaskReasoner -> SocialReasoner -> NLG -> AndroidClient
- * Note: this time you will have to run DialogueSystem and uncomment
+ * This example is pretty similar to EX15_Wholepipeline, however, unlike Ex15_WholePipeline, in this example
+ * the master MUF (Sara MUF) calls its slaves MUF's (i.e., Dialogue MUF, etc.). In order to run this example,
+ * you have to define the connection information of your slaves MUF's into a json file (e.g., services.json)
+ * and set it to the config object.
  */
-public class Ex15_WholePipeline extends MainClass {
+public class Ex16_MasterCallsSlaves extends MainClassNew {
 
-    public static void main(String args[]) {
-        //ConversationalStrategyUtil.preloadRecipes();
-        new Ex15_WholePipeline().execute();
+    public static void main(String args[]) throws Throwable{
+        List<ShutdownHook> hooks = new ArrayList<>();
+        // You can add hooks that will be executed when the MUF is stopped
+        hooks.add( new ShutdownHook() {
+            @Override
+            public void execute() {
+                //TODO: do something
+            }
+        });
+        new Ex16_MasterCallsSlaves().execute( hooks );
     }
 
     @Override
@@ -26,19 +40,21 @@ public class Ex15_WholePipeline extends MainClass {
                 new PluginModule.Builder(SaraOrchestratorEx15.class)
                         //comment out the line below if you want to use remote DialogueSystem
                         .addPlugin(NLUComponent.class, SaraCons.ID_NLU)
-                        //.addPlugin(CSCComponent.class, SaraCons.ID_CSC)
+                        .addPlugin(FakeCSCComponent.class, SaraCons.ID_CSC)
                         //.addPlugin(NLU_DMComponent.class, SaraCons.ID_NLU)
                         //.addPlugin(RapportEstimator.class, SaraCons.ID_RPT)
                         .addPlugin(TaskReasonerComponent.class, SaraCons.ID_DM)
                         .addPlugin(SocialReasonerComponent.class, SaraCons.ID_SR)
                         .addPlugin(NLGComponent.class, SaraCons.ID_NLG)
-                        //.addPlugin(OpenFaceComponent.class, SaraCons.ID_OF)
                         //.addPlugin(R5StreamComponent.class, SaraCons.ID_R5)
                         //.addPlugin(UserModelComponent.class, SaraCons.ID_UM)
                         .build()
         };
+    }
 
+    @Override
+    protected Config createConfig() {
+        return super.createConfig()
+                .setJsonServicesConfig("services.json");
     }
 }
-
-
