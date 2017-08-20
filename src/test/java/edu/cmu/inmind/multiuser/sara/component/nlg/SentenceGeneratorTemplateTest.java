12cca817385441b82216a8548df0f44637a506db
Timo Baumann <baumann@informatik.uni-hamburg.de>
2017-08-20 23:11:17 +0200

    be more flexible about what version of junit we require

---
+++ src/test/java/edu/cmu/inmind/multiuser/sara/component/nlg/SentenceGeneratorTemplateTest.java
@@ -0,0 +1,55 @@
+package edu.cmu.inmind.multiuser.sara.component.nlg;
+
+import edu.cmu.inmind.multiuser.common.Utils;
+import edu.cmu.inmind.multiuser.common.model.ConversationalStrategy;
+import edu.cmu.inmind.multiuser.common.model.DMOutput;
+import edu.cmu.inmind.multiuser.common.model.SROutput;
+import edu.cmu.inmind.multiuser.sara.component.nlg.SentenceGenerator;
+import edu.cmu.inmind.multiuser.sara.component.nlg.SentenceGeneratorTemplate;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+/**
+ * Created by timo on 18.08.17.
+ */
+public class SentenceGeneratorTemplateTest {
+
+    @Test
+    public void testDefault() throws FileNotFoundException {
+        SentenceGenerator gen = new SentenceGeneratorTemplate();
+        String recommendedMovie = "Silver Linings Playbook (2012)";
+        DMOutput dmout = Utils.fromJson("{'action': 'recommend', 'entities': [], 'frame': {'frame': {'actors': {'like': [{'entity': 'actors', 'value': 'jennifer lawrence', 'start': 0, 'end': 17, 'confidence': 1, 'id': 'jennifer_iii_lawrence', 'polarity': 0.0}], 'dislike': []}, 'genres': {'like': [{'entity': 'genres', 'id': 'romance', 'value': 'romantic', 'start': 0, 'end': 8, 'polarity': 0.0}, {'entity': 'genres', 'id': 'comedy', 'value': 'comedy', 'start': 9, 'end': 15, 'polarity': 0.0}], 'dislike': []}, 'directors': {'like': [{'entity': 'directors', 'value': 'david o. russell', 'start': 0, 'end': 13, 'confidence': 0.5, 'id': 'david_o._russell', 'polarity': 0.0}], 'dislike': []}, 'movies': {'like': ['Silver Linings Playbook (2012)'], 'dislike': [], 'history': []}}, 'ask_stack': ['recommend'], 'universals': ['help', 'start_over']}, 'recommendation': {'rexplanations': [{'recommendation': '"+recommendedMovie+"', 'explanations': ['David O. Russell', 'Jennifer (III) Lawrence']}]}}",
+                DMOutput.class);
+        SROutput srOutput = new SROutput();
+        srOutput.setAction(dmout.getAction());
+        srOutput.setEntities(dmout.getEntities());
+        srOutput.setRecommendation(dmout.getRecommendation());
+        srOutput.setUserFrame(dmout.getUserFrame());
+        srOutput.setRapport(7);
+        srOutput.setStrategy("NONE");
+        String nlgout = gen.generate(srOutput);
+        assert(nlgout.contains(recommendedMovie));
+        System.out.println(nlgout);
+        for (ConversationalStrategy cs : ConversationalStrategy.values()) {
+            srOutput.setStrategy(cs.name());
+            nlgout = gen.generate(srOutput);
+            assert(nlgout.contains(recommendedMovie));
+            System.out.println(nlgout);
+        }
+    }
+
+    @Test public void testTestDB() throws FileNotFoundException {
+
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testException1() throws FileNotFoundException {
+        new SentenceGeneratorTemplate("lets/hope/this/file/does/not/exist");
+    }
+    @Test(expected = AssertionError.class)
+    public void testException2() throws FileNotFoundException {
+        new SentenceGeneratorTemplate((InputStream) null);
+    }
+}
