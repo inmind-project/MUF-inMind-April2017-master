a43079ba293c77c5554e75797763d97589f119fb
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2017-09-17 01:33:47 +0000

    do not build or use jar (run directly through application plugin)

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #date=`date --iso-8601=seconds`
 date=`date -u +"%Y-%m-%dT%H:%M:%S"`
-bash ./gradlew build -x test && java -jar build/libs/sara-with-dependencies-2.0.jar  > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
+kill -9 `pgrep java`
+bash ./gradlew run -x test && java -jar build/libs/sara-with-dependencies-2.0.jar  > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
