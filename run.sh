37ff1a5deab526c8bf9f848104ce0f9d379a8eb3
Todd Shore <tshore@cs.cmu.edu>
2018-04-23 16:44:21 -0400

    no longer killing all Java processes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #date=`date --iso-8601=seconds`
 date=`date -u +"%Y-%m-%dT%H:%M:%S"`
-kill -9 `pgrep java` # kill left over java processes
+#kill -9 `pgrep java` # kill left over java processes
 #find ~/.gradle/caches -type f -name '*multiuser*' -exec rm {} \; # reload the multiuser-JARs on every invocation
 bash ./gradlew run -x test > >(tee stdout.$date) 2> >(tee stderr.$date >&2)
 echo "finished running" `date`
