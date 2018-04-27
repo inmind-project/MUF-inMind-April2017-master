301dca45f6b962307b20387c3be0ed06c728aac6
Todd Shore <tshore@cs.cmu.edu>
2018-04-27 16:42:19 -0400

    Revert "updated sarabeat"
    This reverts commit ad87bcd8b3dde2ef6bbf469cca3866cab623c047.

--- src/main/java/edu/cmu/inmind/multiuser/sara/orchestrator/SaraOrchestrator.java
+++ src/main/java/edu/cmu/inmind/multiuser/sara/orchestrator/SaraOrchestrator.java
@@ -1,5 +1,6 @@
 package edu.cmu.inmind.multiuser.sara.orchestrator;
 
+import beat.bson.BSON;
 import edu.cmu.inmind.multiuser.common.SaraCons;
 import edu.cmu.inmind.multiuser.common.model.ASROutput;
 import edu.cmu.inmind.multiuser.common.model.R5StreamListener;
@@ -11,7 +12,6 @@ import edu.cmu.inmind.multiuser.controller.communication.SessionMessage;
 import edu.cmu.inmind.multiuser.controller.log.Log4J;
 import edu.cmu.inmind.multiuser.controller.orchestrator.ProcessOrchestratorImpl;
 import edu.cmu.inmind.multiuser.controller.session.Session;
-import edu.cmu.inmind.multiuser.sara.component.beat.bson.BSON;
 
 /**
  * Created by oscarr on 3/3/17.
